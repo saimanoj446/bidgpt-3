f19c5b1514a7b2ec9c7980bbe026a42401c6456e
saimanoj446 <peketiveeravenkatasatyannaraya@gmail.com>
2025-06-13 20:31:00 +0530

    Fixed Pinecone initialization in chat script

--- pinecone-chat.js
+++ pinecone-chat.js
@@ -23,7 +23,7 @@ class BidGPTChatbot {
             console.log('Initializing Pinecone...');
             
             // Initialize Pinecone with browser version
-            const pinecone = new PineconeClient({
+            const pinecone = new Pinecone({
                 apiKey: this.apiKey,
                 environment: 'gcp-starter'
             });
